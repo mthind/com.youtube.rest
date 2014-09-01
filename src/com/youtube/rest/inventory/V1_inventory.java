5aadd9b410069c29524116ba30fc6a925684cffc
mthind <mthind@ptc.com>
2014-09-01 16:12:39 +0530

    JSON parsing of data from DB

---
+++ src/com/youtube/rest/inventory/V1_inventory.java
@@ -0,0 +1,60 @@
+package com.youtube.rest.inventory;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.QueryParam;
+
+import org.codehaus.jettison.json.JSONArray;
+
+import com.youtube.dao.Oracle308tube;
+import com.youtube.utilities.ToJSON;
+
+@Path("/v1/inventory")
+public class V1_inventory {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String returnAllPcParts() throws Exception {
+		
+		PreparedStatement query = null;
+		Connection conn = null;
+		String returnString = null;
+		
+		try {
+			conn = Oracle308tube.Oracle308tubeConn().getConnection();
+			query = conn.prepareStatement("select * " +
+											"from PC_PARTS");
+			ResultSet resultSet = query.executeQuery();
+			
+			// get an instance of the converter
+			ToJSON converter = new ToJSON();
+			
+			//create json array ref			
+			JSONArray jsonArray = new JSONArray();
+			
+			// parse the resultSet
+			jsonArray = converter.toJSONArray(resultSet);
+			
+			query.close(); // close the connection!!
+			
+			returnString = jsonArray.toString();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally{
+			if(conn != null){
+				conn.close();
+			}
+		}
+		return returnString;
+	}
+}
---
+++ src/com/youtube/utilities/ToJSON.java
@@ -0,0 +1,113 @@
+package com.youtube.utilities;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
+import org.owasp.esapi.ESAPI;
+
+import java.sql.ResultSetMetaData;
+
+import java.sql.ResultSet;
+import java.sql.Types;
+/*
+ * Send the data set from 
+ * inventory class to this class. 
+ * This will be doing the
+ * parsing for us and returning the 
+ * JSON data.
+ */
+public class ToJSON {
+
+	public JSONArray toJSONArray(ResultSet resultSet) throws Exception{
+		JSONArray jsonArray = new JSONArray(); // JSON array that will be returned
+
+		//save the table data(column names, column count)
+		ResultSetMetaData rsmd = resultSet.getMetaData();
+
+		// loop through EACH ROW
+		while (resultSet.next()){
+
+			// figure out the columns count
+			int numColumns = rsmd.getColumnCount();
+
+			String temp = null;
+			//convert each row to a JSON object			
+			JSONObject obj = new JSONObject();
+
+			// We loop through EACH COLUMN 
+			// and place data in object
+			for(int i = 1; i <=numColumns; i++){
+				String column_name = rsmd.getColumnName(i);
+
+				if(rsmd.getColumnType(i) == Types.ARRAY){
+					obj.put(column_name, resultSet.getArray(column_name));
+					/*Debug */ System.out.println("ToJSON : Array");
+				}
+				else if(rsmd.getColumnType(i)==java.sql.Types.BIGINT){
+					obj.put(column_name, resultSet.getInt(column_name));
+					/*Debug*/ System.out.println("ToJson: BIGINT");
+				}
+				else if(rsmd.getColumnType(i)==java.sql.Types.BOOLEAN){
+					obj.put(column_name, resultSet.getBoolean(column_name));
+					/*Debug*/ System.out.println("ToJson: BOOLEAN");
+				}
+				else if(rsmd.getColumnType(i)==java.sql.Types.BLOB){
+					obj.put(column_name, resultSet.getBlob(column_name));
+					/*Debug*/ System.out.println("ToJson: BLOB");
+				}
+				else if(rsmd.getColumnType(i)==java.sql.Types.DOUBLE){
+					obj.put(column_name, resultSet.getDouble(column_name));
+					/*Debug*/ System.out.println("ToJson: DOUBLE");
+				}
+				else if(rsmd.getColumnType(i)==java.sql.Types.FLOAT){
+					obj.put(column_name, resultSet.getFloat(column_name));
+					/*Debug*/ System.out.println("ToJson: FLOAT");
+				}
+				else if(rsmd.getColumnType(i)==java.sql.Types.INTEGER){
+					obj.put(column_name, resultSet.getInt(column_name));
+					/*Debug*/ System.out.println("ToJson: INTEGER");
+				}
+				else if(rsmd.getColumnType(i)==java.sql.Types.NVARCHAR){
+					obj.put(column_name, resultSet.getNString(column_name));
+					/*Debug*/ System.out.println("ToJson: NVARCHAR");
+				}
+				else if(rsmd.getColumnType(i)==java.sql.Types.VARCHAR){
+					temp = resultSet.getString(column_name); //saving column data to temp variable
+					temp = ESAPI.encoder().canonicalize(temp); //decoding data to base state
+					temp = ESAPI.encoder().encodeForHTML(temp); //encoding to be browser safe
+					obj.put(column_name, temp); //putting data into JSON object
+					//obj.put(column_name, rs.getString(column_name));
+					// /*Debug*/ System.out.println("ToJson: VARCHAR");
+				}
+				else if(rsmd.getColumnType(i)==java.sql.Types.TINYINT){
+					obj.put(column_name, resultSet.getInt(column_name));
+					/*Debug*/ System.out.println("ToJson: TINYINT");
+				}
+				else if(rsmd.getColumnType(i)==java.sql.Types.SMALLINT){
+					obj.put(column_name, resultSet.getInt(column_name));
+					/*Debug*/ System.out.println("ToJson: SMALLINT");
+				}
+				else if(rsmd.getColumnType(i)==java.sql.Types.DATE){
+					obj.put(column_name, resultSet.getDate(column_name));
+					/*Debug*/ System.out.println("ToJson: DATE");
+				}
+				else if(rsmd.getColumnType(i)==java.sql.Types.TIMESTAMP){
+					obj.put(column_name, resultSet.getTimestamp(column_name));
+					/*Debug*/ System.out.println("ToJson: TIMESTAMP");
+				}
+				else if(rsmd.getColumnType(i)==java.sql.Types.NUMERIC){
+					obj.put(column_name, resultSet.getBigDecimal(column_name));
+					// /*Debug*/ System.out.println("ToJson: NUMERIC");
+				}
+				else {
+					obj.put(column_name, resultSet.getObject(column_name));
+					/*Debug*/ System.out.println("ToJson: Object "+column_name);
+				} 
+			}
+
+			// add the object to array
+			jsonArray.put(obj);
+		}
+		return jsonArray;
+
+	}
+}
