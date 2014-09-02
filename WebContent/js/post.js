8a080158f1b01066e1f1b3c7232966eee5c8adf3
mthind <mansukhdeepsingh@yahoo.co.in>
2014-09-02 20:31:55 +0530

    added  function serializeObject()
    Added function that was required as a part of post.js

--- WebContent/js/post.js
+++ WebContent/js/post.js
@@ -6,6 +6,24 @@
  *
  * jquery is required
  */
+
+$.fn.serializeObject = function()
+{
+   var o = {};
+   var a = this.serializeArray();
+   $.each(a, function() {
+       if (o[this.name]) {
+           if (!o[this.name].push) {
+               o[this.name] = [o[this.name]];
+           }
+           o[this.name].push(this.value || '');
+       } else {
+           o[this.name] = this.value || '';
+       }
+   });
+   return o;
+};
+
 $(document).ready(function() {
 //	console.log("ready");
 	var $post_example = $('#post_example');
